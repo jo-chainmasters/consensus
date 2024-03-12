399263e876c02fbbf86fc89f8d4134cf8e4d341e
Jo | Chainmasters <jo.chainmasters@gmail.com>
2024-03-12 07:59:40 +0100

    1st try to add a consensus visualizer

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
-import { HttpClient } from "@angular/common/http";
-import { forkJoin } from "rxjs";
-import { BlockHeaderComponent } from "../components/block-header/block-header.component";
-import { PrevotesComponent } from "../components/prevotes/prevotes.component";
+import { HttpClient } from '@angular/common/http';
+import { forkJoin, timestamp } from 'rxjs';
+import { BlockHeaderComponent } from '../components/block-header/block-header.component';
+import { PrevotesComponent } from '../components/prevotes/prevotes.component';
+import { TreeTableModule } from 'primeng/treetable';
+import { TreeNode } from 'primeng/api';
 
 const baseUrlApp = 'http://localhost:4000/blocks/';
 const baseUrlRpc = 'https://rpc1.unification.io/';
@@ -13,7 +15,13 @@ const baseUrlRest = 'https://rest.unification.io/';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableModule, BlockHeaderComponent, PrevotesComponent],
+  imports: [
+    RouterOutlet,
+    TableModule,
+    BlockHeaderComponent,
+    PrevotesComponent,
+    TreeTableModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -22,11 +30,9 @@ export class AppComponent implements OnInit {
 
   block: any;
   rpcBlock: any;
-  rounds: string[] = [];
+  _rounds: string[] = [];
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     const obsConsensus = this.getConsensus(9522614);
@@ -35,31 +41,65 @@ export class AppComponent implements OnInit {
     forkJoin([obsConsensus, obsCommit]).subscribe((result: any[]) => {
       this.block = result[0];
       this.rpcBlock = result[1].result.signed_header;
-      this.rounds = Object.keys(this.block.votes.prevote);
-      console.log(this.rounds);
+      this._rounds = Object.keys(this.block.votes.prevote);
     });
-
   }
 
-  get precommits() {
-    if(this.block)
-      return this.block.votes.precommit['0'];
-    else
-      return [];
+  get bla() {
+    const rounds = [];
+    for (const round of this._rounds) {
+      const node: TreeNode = {
+        data: {
+          validator: 'Round 0',
+          timestamp: 'timestamp',
+          type: 'Folder',
+        },
+        children: [],
+      };
+      rounds.push(node);
+
+      const steps = ['prevote', 'precommit', 'commit'];
+      for (const step of steps) {
+        const stepNode: TreeNode = {
+          data: {
+            validator: step,
+            timestamp: 'timestamp',
+          },
+          children: [],
+        };
+        node.children?.push(stepNode);
+      }
+
+      // const value = this.block.rounds[round][step];
+      // for (const v of value) {
+      //   stepNode.children?.push({
+      //     validator: v.validator,
+      //     timestamp: v.timestamp,
+      //     type: 'Application',
+      //   });
+      // }
+    }
+
+    console.log(rounds);
+    return rounds;
+  }
+
+  getRounds() {
+    return this._rounds;
   }
 
   getPrevotes(round: string) {
-    console.log('round: ' + round);
-    if(this.block) {
-      console.log(this.block.votes.prevote[round]);
+    if (this.block) {
       return this.block.votes.prevote[round];
-    } else
+    } else {
       return [];
+    }
   }
 
   get commits() {
     return this.rpcBlock.commit.signatures;
   }
+
   private getConsensus(blockHeight: number) {
     return this.getHttp(baseUrlApp + blockHeight);
   }
@@ -71,5 +111,4 @@ export class AppComponent implements OnInit {
   private getHttp(url: string) {
     return this.http.get(url);
   }
-
 }
